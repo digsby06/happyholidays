3d7d00357fa78c515f3fb4ee043e3b0415b0010e
TERRENCE WHITTAKER <terr.whittakerjr@gmail.com>
2016-10-26 16:48:15 -0400

    Created ambient mode for events

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,30 +2,43 @@ module Api
   class EventsController < ApplicationController
     before_action :set_event, only: [:show, :update, :destroy]
 
+    thr = Thread.new {
+          puts "thread"
+          timer
+          puts "timer is running"
+      }
+    $last_play_time = Time.now
+
     # GET /events
     def index
       @events = Event.all
-      # Event.start_show
       render json: @events , status: 200
     end
 
     # GET /events/1
     def show
       render json: @event.trigger , status: 200
-
     end
 
     # POST /events
     def create
       @event = Event.new(event_params)
 
+      can_play = true
+
       if @event.save
-        # Event.start_show
-        require "mixpanel-ruby"
-        tracker = Mixpanel::Tracker.new("9ae4dc350f5c0a7ccf5a0cff913c4a4d")
+        if can_play == true
+          can_play = false
+          Event.start_show
 
-        tracker.track(@event.id, 'Event Triggered')
 
+        # Mixpanel is used to track each time a gesture event is triggered
+          require "mixpanel-ruby"
+          tracker = Mixpanel::Tracker.new("9ae4dc350f5c0a7ccf5a0cff913c4a4d")
+          tracker.track(@event.id, 'Event Triggered')
+
+          can_play = true
+        end
         render json: @event, status: :created
       else
         render json: @event.errors, status: :unprocessable_entity
@@ -47,6 +60,41 @@ module Api
     def destroy
       @event.destroy
     end
+
+
+    def self.start_ambient_mode
+      puts 'Now Starting Ambient Mode'
+      Event.start_show
+    end
+
+    def self.timer
+      puts 'Inside the timer block'
+      @@time_check_never_stop = true
+      puts @@time_check_never_stop
+        while @@time_check_never_stop
+            puts 'loop is working'
+            compare_time
+            sleep(5)
+            puts 'Time Compared'
+        end
+        puts "test123"
+    end
+
+
+    def self.compare_time
+      puts 'inside time comparison block'
+      time = Time.now
+      puts time
+      puts $last_play_time + 30
+      if time >= ($last_play_time + 30)
+        puts 'Enough time has passed without an event'
+        start_ambient_mode
+      else
+        puts 'Not this time'
+      end
+
+    end
+
 
     private
       # Use callbacks to share common setup or constraints between actions.
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,29 @@
 class Event < ApplicationRecord
+
+  def self.start_show
+
+    puts 'The Show has started'
+    # Play Audio Track
+    # if vlc.connected? == false
+    #   vlc = VLC::System.new
+    # end
+    # vlc.play('https://att_holiday.dev/assets/audios/4936.mp3')
+
+    # Connect to Stratacache
+
+      # Waiting for information - Place code here
+
+    # Connect to Cue server to start light show
+
+    # sock = UDPSocket.new
+    # data = 'something to be determined'
+    # sock.send(data, 0, '172.16.2.210', 52737 )
+    # sock.close
+
+    $last_play_time = Time.now
+    puts 'The Show has ended'
+    puts $last_play_time
+  end
+
+
 end
